b5a85edb496646c9dfea6b8fd9d4c02b8e6891a2
dubovcev <dubovcev@gmail.com>
2020-03-10 11:36:35 +0500

    Делаем форму отправки статьи на почту

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm
 
 def post_list(request):
     object_list = Post.published.all()
@@ -26,3 +27,17 @@ class PostListView(ListView):
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+def post_share(request, post_id):
+    # Получение статьи по идентификатору
+    post = get_object_or_404(Post, id=post_id, status = 'published')
+    if request.method == 'POST':
+        # Форма была отправлена на сохранение
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Все поля формы прошли валидацию
+            cd = form.cleaned_data
+            # Отправка электронной почты
+        else:
+            form = EmailPostForm()
+            return render(request, 'blog/post/share.html', {'post': post, 'form': form})
